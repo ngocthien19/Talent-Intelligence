79be87cd4a7de097f4b6ceedab1897ea84e0aea1
ngocthien19 <caongocthien1902@gmail.com>
2026-07-16 22:06:37 +0700

    complete auth

--- WEB_AI_Powered_TI/src/layouts/candidate/CandidateLayout.jsx
+++ WEB_AI_Powered_TI/src/layouts/candidate/CandidateLayout.jsx
@@ -1,8 +1,22 @@
+import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { getProfile } from '~/redux/slices/auth.slice'
 import Header from './Header'
 import Footer from './Footer'
 
 const CandidateLayout = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const hasAccessToken = document.cookie.includes('accessToken')
+    const userInStorage = localStorage.getItem('user')
+
+    if (hasAccessToken && userInStorage) {
+      dispatch(getProfile())
+    }
+  }, [dispatch])
+
   return (
     <div className="app-wrapper">
       <Header />
