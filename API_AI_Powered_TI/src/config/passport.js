788b1bde1ec2db0326cce6559eb89d154a1424a2
ngocthien19 <caongocthien1902@gmail.com>
2026-07-16 21:54:14 +0700

    feat: login by google

--- API_AI_Powered_TI/src/config/passport.js
+++ API_AI_Powered_TI/src/config/passport.js
@@ -1,6 +1,6 @@
 import passport from 'passport'
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
-import { env } from './environment.js'
+import { env } from './environment'
 import authService from '~/services/auth/auth.service'
 
 passport.use(
@@ -8,12 +8,11 @@ passport.use(
     {
       clientID: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
-      callbackURL: env.GOOGLE_CALLBACK_URL || 'http://localhost:3000/api/auth/google/callback',
+      callbackURL: env.GOOGLE_CALLBACK_URL,
       passReqToCallback: true
     },
     async (req, accessToken, refreshToken, profile, done) => {
       try {
-
         const userData = {
           googleId: profile.id,
           email: profile.emails?.[0]?.value,
@@ -23,12 +22,14 @@ passport.use(
 
         const result = await authService.googleLogin(userData)
 
-        req.user = result.user
-        req.tokens = result.tokens
+        req.accessToken = result.tokens.accessToken
+        req.refreshToken = result.tokens.refreshToken
 
-        return done(null, result.user)
+        return done(null, result.user, {
+          accessToken: result.tokens.accessToken,
+          refreshToken: result.tokens.refreshToken
+        })
       } catch (error) {
-        console.error('Google strategy error:', error)
         return done(error, null)
       }
     }
